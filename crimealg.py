5916dbcb77d790737c96093a7307e62258b4faa2
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2022-03-08 18:12:27 +0000

    Update crimealg.py

--- crimealg.py
+++ crimealg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-#  Copyright 2022 sixie6e <sixie6e@parrot>
+#  Copyright 2022 sixie6e <sixie6e@distherapy>
 #
 #  People really love algorithms. Here's one to show you, mathematically how law enforcement does not create a healthy, moral, or civilized society but a fearful, oppressed, revenue generating one.
 #
@@ -8,45 +8,58 @@
 import numpy
 import pandas
 import matplotlib
-
+# 2020 
 def federal():
+	t0=1
+	t1=t0*30
+	t2=t1*6
+	t3=t2*2+5
+	t4=t3*10
 	a=76656 #convictions
-	b= a-(.97*a) #plea_deals
+	b=a-(.97*a) #plea_deals
 	c=a-b #trial_convictions
-	d=  #dismissed
-	e=  #not_charged
-	f=   
-	g=
-	h=
-	i=
-	j=
-	k=
-	l=
-	t0=24
+	d=  #trials
+	e=  #dismissed
+	f=  #not_charged 
+	g=  #num_laws
+	h=  #num_enforcers
+	i=  #num_facilities
+	j=  #num_guards
+	k=  #num_inmates
+	l=  #population
+	#(m-o) based on the most severe. i.e. a home invasion where the depraved tie up people for their money is all three but is more violent than n or o.
+	m=  #violent_crime
+	n=  #property_crime
+	o=  #financial_crime
+	p=t1*40  #sentence
+
+# 2020
+def  state():
+	t0=1
 	t1=t0*90
 	t2=t1*2
 	t3=t2*2+5
 	t4=t3*10
-
-def  state():
 	a= #convictions
 	b= a-(.97*a) #plea_deals
 	c=a-b #trial_convictions
-	d=  #dismissed
-	e=  #not_charged
-	f=
-	g=
-	h=
-	i=
-	j=
-	k=
-	l=
-	t0=24
-	t1=t0*90
-	t2=t1*2
-	t3=t2*2+5
-	t4=t3*10
+	d=  #trials
+	e=  #dismissed
+	f=  #not_charged 
+	g=  #num_laws
+	h=  #num_enforcers
+	i=  #num_facilities
+	j=  #num_guards
+	k=  #num_inmates
+	k=  #num_inmates
+	l=  #population
+	#(m-o) based on the most severe. i.e. a home invasion where the depraved tie up people for their money is all three but is more violent than n or o.
+	m=  #violent_crime
+	n=  #property_crime
+	o=  #financial_crime
+	p=  #sentence
 
+# 2020
 def corporate():
 	a= #phone calls
 	b= #video visits
