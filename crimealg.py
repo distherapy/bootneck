bf57f249339aeee3e5bc6404bab12c780b8ba38e
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2022-03-08 16:29:41 +0000

    Add files via upload

---
+++ crimealg.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+#
+#  Copyright 2022 sixie6e <sixie6e@parrot>
+#
+#  People really love algorithms. Here's one to show you, mathematically how law enforcement does not create a healthy, moral, or civilized society but a fearful, oppressed, revenue generating one.
+#
+#
+import numpy
+import pandas
+import matplotlib
+
+def federal():
+	a=76656 #convictions
+	b= a-(.97*a) #plea_deals
+	c=a-b #trial_convictions
+	d=  #dismissed
+	e=  #not_charged
+	f=   
+	g=
+	h=
+	i=
+	j=
+	k=
+	l=
+	t0=24
+	t1=t0*90
+	t2=t1*2
+	t3=t2*2+5
+	t4=t3*10
+
+def  state():
+	a= #convictions
+	b= a-(.97*a) #plea_deals
+	c=a-b #trial_convictions
+	d=  #dismissed
+	e=  #not_charged
+	f=
+	g=
+	h=
+	i=
+	j=
+	k=
+	l=
+	t0=24
+	t1=t0*90
+	t2=t1*2
+	t3=t2*2+5
+	t4=t3*10
+
+def corporate():
+	a= #phone calls
+	b= #video visits
+	c= #commissary
+	t0=24
+	rev_gen=(a+b+c)*t0
