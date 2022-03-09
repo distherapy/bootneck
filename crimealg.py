f85a0aa3bd3d812823e2b146add1fda16bb9fb02
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2022-03-09 12:03:58 +0000

    Update crimealg.py

--- crimealg.py
+++ crimealg.py
@@ -21,13 +21,13 @@ def federal():
 	d=  #trials
 	e=  #dismissed
 	f=  #not_charged 
-	g=  #num_laws
-	h=  #num_enforcers
-	i=  #num_facilities
-	j=  #num_guards
-	k=  #num_admin
+	g=88899  #num_laws/rules/regs
+	h=1400000+35000+4000+10100+1700+30000+20000 (military,fbi,usm,dea,atf,nsa,cia)  #num_enforcers
+	i=122 #num_facilities
+	j=38000  #num_guards
+	k=153000 #num_prisoners
 	l=  #population
-	#(m-o) based on the most severe. i.e. a home invasion where the depraved tie up people for their money is all three but is more violent than n or o.
+	#(m-o) based on the most severe. i.e. a home invasion where the depraved tie up people for their money is all three but is more violent than it is n or o.
 	m=  #violent_crime
 	n=  #property_crime
 	o=  #financial_crime
@@ -43,18 +43,44 @@ def  state():
 	t3=t2*2+5
 	t4=t3*10
 	a= #convictions
-	b=.97(a) #plea_deals
+	b=.95(a) #plea_deals
 	c=a-b #trial_convictions
 	d=  #trials
 	e=  #dismissed
 	f=  #not_charged 
-	g=  #num_laws
-	h=  #num_enforcers
+	g=6000  #num_laws
+	h=900000  #num_enforcers from policebluenation.org
+	i=3100  #num_facilities
+	j=400000 #num_guards
+	k=12000000 #num_prisoners
+	l=  #population
+	#(m-o) based on the most severe. i.e. a home invasion where the depraved tie up people for their money is all three but is more violent than it is n or o.
+	m=  #violent_crime
+	n=  #property_crime
+	o=  #financial_crime
+	p=t3*15  #sentence
+	q=i*t3*(util+basic_needs +(47509(j)))  #overhead
+	r=  #revgen
+# 2020
+def  municipal():
+	t0=1
+	t1=t0*90
+	t2=t1*2
+	t3=t2*2+5
+	t4=t3*10
+	a= #convictions
+	b=.99(a) #plea_deals
+	c=a-b #trial_convictions
+	d=  #trials
+	e=  #dismissed
+	f=  #not_charged 
+	g=1500  #num_laws
+	h=900000 #num_enforcers
 	i=  #num_facilities
 	j=  #num_guards
-	k=  #num_admin
+	k=  #num_prisoners
 	l=  #population
-	#(m-o) based on the most severe. i.e. a home invasion where the depraved tie up people for their money is all three but is more violent than n or o.
+	#(m-o) based on the most severe. i.e. a home invasion where the depraved tie up people for their money is all three but is more violent than it is n or o.
 	m=  #violent_crime
 	n=  #property_crime
 	o=  #financial_crime
@@ -72,7 +98,7 @@ def corporate():
 	a=1400000000 #phone calls in us dollars
 	b=12.95*52*k #video visits
 	c=1700000000 #commissary in us dollars
-	d=a+b+c  #revgen
+	d=(a+b+c) #revgen
 
 for y in range(2024,2040):
 	revgen = z2*r-z1*q
