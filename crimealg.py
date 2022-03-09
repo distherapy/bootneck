092d51fc5bcb552bb7de7a715f376053fa33e901
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2022-03-09 09:25:03 +0000

    Create crimealg.py

---
+++ crimealg.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+#
+#  Copyright 2022 sixie6e <sixie6e@distherapy>
+#
+#  Law enforcement does not create a healthy, moral, or civilized society but a fearful, oppressed, revenue generating one.
+#
+#
+import numpy
+import pandas
+import matplotlib
+# 2020 
+def federal():
+	t0=1
+	t1=t0*30
+	t2=t1*6
+	t3=t2*2+5
+	t4=t3*10
+	a=76656 #convictions
+	b=a-(.97*a) #plea_deals
+	c=a-b #trial_convictions
+	d=  #trials
+	e=  #dismissed
+	f=  #not_charged 
+	g=  #num_laws
+	h=  #num_enforcers
+	i=  #num_facilities
+	j=  #num_guards
+	k=  #num_inmates
+	l=  #population
+	#(m-o) based on the most severe. i.e. a home invasion where the depraved tie up people for their money is all three but is more violent than n or o.
+	m=  #violent_crime
+	n=  #property_crime
+	o=  #financial_crime
+	p=t1*40  #sentence
+	q=(i*util*t3*basic_needs)*(45779(j))  #overhead
+	r=gross-q  #revgen
+
+# 2020
+def  state():
+	t0=1
+	t1=t0*90
+	t2=t1*2
+	t3=t2*2+5
+	t4=t3*10
+	a= #convictions
+	b= a-(.97*a) #plea_deals
+	c=a-b #trial_convictions
+	d=  #trials
+	e=  #dismissed
+	f=  #not_charged 
+	g=  #num_laws
+	h=  #num_enforcers
+	i=  #num_facilities
+	j=  #num_guards
+	k=  #num_inmates
+	l=  #population
+	#(m-o) based on the most severe. i.e. a home invasion where the depraved tie up people for their money is all three but is more violent than n or o.
+	m=  #violent_crime
+	n=  #property_crime
+	o=  #financial_crime
+	p=t3*15  #sentence
+	q=(i*util*t3*basic_needs)*(47509(j))  #overhead
+	r=  #revgen
+
+# 2020
+def corporate():
+	a=1400000000 #phone calls in us dollars
+	b=12.95*52*k #video visits
+	c=1700000000 #commissary in us dollars
+	t0=24
+	d=(a+b+c)*t3  #revgen
+
+for y in range(2024,2040):
+	revgen = z2*r-z1*q
+	print(y + ' = ' + revgen')
+	
